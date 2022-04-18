c5f67992ca9f12f45e80c7b698528f622a433f1f
Arjin <96173187+ArjinAlbay@users.noreply.github.com>
2022-04-18 21:49:59 +0300

    Create degisken-tanimlama.js
    eklendi

---
+++ degisken-tanimlama.js
@@ -0,0 +1,14 @@
+let yonetici;
+let isim;
+
+isim = "ahmet";
+yonetici = isim;
+
+console.log(yonetici);
+
+alert(yonetici);
+
+let gezegenimizinAdi = "Dünya";
+let simdikiKullanici = " mehmet ";
+
+console.log(gezegenimizinAdi + simdikiKullanici);
