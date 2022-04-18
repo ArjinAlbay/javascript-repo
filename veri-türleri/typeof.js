9552778b7fb9d227a1e9a248db73a13eb156c397
Arjin <96173187+ArjinAlbay@users.noreply.github.com>
2022-04-18 21:48:38 +0300

    Create typeof.js
    eklendi

---
+++ veri-türleri/typeof.js
@@ -0,0 +1,13 @@
+// typeof argüman tipini bildirir. Farklı tipler için farklı akışlarınız varsa bunu kullanabilirsiniz.
+
+// İki türlü yazımı vardır:
+
+// Operatör olarak: typeof x.
+// Fonksiyonel tipte: typeof(x).
+// Diğer bir deyişle parantezli de çalışır parantez olmadan da çalışır. Sonuç aynı olacaktır.
+
+// typeof x'i çalıştırdığınızda bu fonksiyon karakter dizisi(String) dönderir:
+
+typeof undefined; // "undefined"
+typeof 0; // "number"
+typeof "foo"; // "string"
