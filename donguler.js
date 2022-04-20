aeb1eb6597ccf90b3a3100802dd37e176f51eac0
Arjin <96173187+ArjinAlbay@users.noreply.github.com>
2022-04-20 21:01:22 +0300

    döngüler eklendi

--- donguler.js
+++ donguler.js
@@ -1,5 +1,7 @@
 //  Loops => Döngüler
 
+// for
+
 for (let i = 0; i <= 5; i++) {
   console.log(i);
 } // 0 1 2 3 4 5
@@ -22,17 +24,71 @@ let countries = ["Finland", "Sweden", "Denmark", "Norway", "Iceland"];
 
 let newAr = [];
 
-for (let i = 0; i <= countries.length; i++) {
+for (let i = 0; i < countries.length; i++) {
   newAr.push(countries[i].toUpperCase());
 }
-// ["FINLAND", "SWEDEN", "DENMARK", "NORWAY", "ICELAND"]
+console.log(newAr);
+// FINLAND,SWEDEN,DENMARK,NORWAY,ICELAND,
 
 let numbers = [1, 2, 3, 4, 5, 6];
 
 let sum = 0;
 
-for (let i = 0; i <= numbers.length; i++) {
+for (let i = 0; i < numbers.length; i++) {
   sum += numbers[i];
 }
 
-console.log(sum);
+console.log(sum); //21
+
+// while
+
+let i = 0;
+
+while (i <= 5) {
+  console.log(i);
+  i++;
+}
+
+//do while
+
+let isimler = [`arjin`, `albay`, `mehmet`];
+
+do {
+  console.log(`isim`, isimler[i]);
+  i++;
+} while (i < isimler.length);
+
+// for of loop
+
+let names1 = [`arjin`, `albay`, `mehmet`];
+
+for (let name of names1) {
+  console.log(name);
+}
+
+for (let isimba of names1) {
+  console.log(isimba[0]);
+}
+// "a" "a" "m"
+
+//break
+
+let names2 = [`arjin`, `albay`, `mehmet`];
+
+for (let namee of names2) {
+  if (namee == `mehmet`) {
+    console.log(`${namee} bulundu`);
+    break;
+  }
+  console.log(namee);
+}
+
+let names3 = [`arjin`, `albay`, `mehmet`];
+
+for (let namee of names3) {
+  if (namee == `albay`) {
+    console.log(`${namee} atlandı`);
+    continue;
+  }
+  console.log(namee);
+}
