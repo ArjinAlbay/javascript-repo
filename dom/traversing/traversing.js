6ee79135da5f8261cd03f4a6de96f6840423eb6c
Arjin <96173187+ArjinAlbay@users.noreply.github.com>
2022-04-29 13:34:54 +0300

    dom elementleri üzerinde gezinme eklendi

--- dom/traversing/traversing.js
+++ dom/traversing/traversing.js
@@ -9,4 +9,18 @@ sonuc = ul.children[0];
 sonuc = ul.firstElementChild;
 sonuc = ul.lastElementChild;
 
+// parentElement => id kısmını yazıp bi üstündeki parentine ulaşabiliriz.
+
+sonuc = document.getElementById("titleid").parentElement;
+
+// console.log(sonuc);
+
+// nextElementSibling, previousElementSibling => eş değer id deki elementlere ulaşabiliriz. kısaca kardeşlerini bulacağız.
+
+sonuc =
+  document.getElementById("titleid").nextElementSibling.nextElementSibling
+    .previousElementSibling;
+
+// istediğimiz kadar kullanabiliriz. ilk yazdığımız bize ikinci kardeşi getirdi. 2. yazdığımız ise 3. kardeşe gitti. son yazdığımız ise önceki kardeşe yani 2. kardeşe geri götürdü.
+
 console.log(sonuc);
