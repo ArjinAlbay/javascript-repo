100e927c2cffa0a71341e518399d917084fb3838
Arjin <96173187+ArjinAlbay@users.noreply.github.com>
2022-06-23 09:54:26 +0300

    clouser file added

---
+++ closure.js
@@ -0,0 +1,37 @@
+// Closure
+
+function outerFunction() {
+  let count = 0;
+  function innerFunction() {
+    count++;
+    return count;
+  }
+
+  return innerFunction;
+}
+const innerFunc = outerFunction();
+
+console.log(innerFunc());
+console.log(innerFunc());
+console.log(innerFunc());
+
+function outerFunc() {
+  let count = 0;
+  function plusOne() {
+    count++;
+    return count;
+  }
+  function minusOne() {
+    count--;
+    return count;
+  }
+
+  return {
+    plusOne: plusOne(),
+    minusOne: minusOne(),
+  };
+}
+let innerFuncs = outerFunc();
+
+console.log(innerFuncs.plusOne);
+console.log(innerFuncs.minusOne);
