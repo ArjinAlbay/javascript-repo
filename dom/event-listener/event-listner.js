ceef78d3c46a802fc75e068141f8fa528b92807e
Arjin <96173187+ArjinAlbay@users.noreply.github.com>
2022-06-23 14:54:35 +0300

    The file has been edited.

--- dom/event-listener/event-listner.js
+++ dom/event-listener/event-listner.js
@@ -1,1 +1,12 @@
-document.querySelector("btnAddNewTask");
+let newTask = (event) => {
+  console.log(`clicked ekle`);
+  event.preventDefault(); // sayfanın yenilenmesini engeller
+};
+
+let clearTask = () => {
+  console.log(`clear temizle`);
+};
+
+document.querySelector("#btnAddNewTask").addEventListener("click", newTask);
+
+document.querySelector(`#btnClear`).addEventListener("click", clearTask);
