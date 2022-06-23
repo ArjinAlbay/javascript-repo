3ce53b8fcb2c367d5d7064f1453db4805c7304c9
Arjin <96173187+ArjinAlbay@users.noreply.github.com>
2022-06-23 09:55:52 +0300

    Some codes have been reviewed

--- dom/tekil-element/tekilElement.js
+++ dom/tekil-element/tekilElement.js
@@ -1,18 +1,37 @@
 let sonuc;
 
-sonuc = document.getElementById("first-title").id; // id sı title olanı bulmak için kullanırız.
-sonuc = document.getElementById("first-title").className; // class adını verir
-sonuc = document.getElementById("first-title").classList; // iki yada üç class olabilir. bize dizi olarak verir sonucu
+sonuc = document.getElementById(`first-title`);
+// elementin tümünü alır => h1#first-title.title
+
+sonuc = document.getElementById(`first-title`).id;
+// sadece id adını aldı. => first-title
+
+sonuc = document.getElementById(`first-title`).className;
+// sadece id si first-title olan ilk elementin classname özelliğini getirdi. => title
+
+sonuc = document.getElementById(`first-title`).classList;
+// sadece id si first-title olan ilk elementin classname listesini getirdi. => title
+
+sonuc = document.getElementsByClassName(`title`);
+// class adı title olan listeyi getirir.
+
+sonuc = document.getElementsByTagName(`h1`); // h1 tagı bulunan tüm listeyi getirir.
 
 document.getElementById("first-title").style.fontSize = "15px";
 document.getElementById("first-title").style.color = "gray";
 
 document.getElementById("first-title").innerText; // içeriği değiştirebilirsin ama bir başlığı paragraf yapamazsın. içerik şekli değil içerik değişir.
-document.getElementById("first-title").innerHTML = "<p>iyiyim sen</p>"; // içerik şekli değişir. h1 p etiketine dönüştü.
+document.getElementById("first-title").innerHTML =
+  "<p>innerhtml ile başlık paragrafa dönüştü</p>"; // içerik şekli değişir. h1 p etiketine dönüştü.
 
-sonuc = document.querySelector("#second-title"); // bulduğu ilk id
-sonuc = document.querySelector(".title"); // bulduğu ilk class
-sonuc = document.querySelector("div"); // bulduğu ilk etiket
+sonuc = document.querySelector(`#second-title`); // id si second-title olan ilk elemanı seçip getirdi.
+
+sonuc = document.querySelector(`.title`); // class adı title olan ilk elemanı seçip getirdi.
+
+sonuc = document.querySelector(`div`); // bulduğu ilk div etiketini alıp getirdi.
+
+sonuc = document.querySelectorAll(`.title`);
+// class adı title olan tüm classları listeler.
 
 sonuc = document.querySelector("li");
 sonuc = document.querySelector("li:first-child");
