5e22dddfe2a0b74b545e3bb554feeeb65fc204c6
Arjin <96173187+ArjinAlbay@users.noreply.github.com>
2022-06-23 11:35:12 +0300

    some code reviewed

--- dom/dom-elementi-olusturma/creating.js
+++ dom/dom-elementi-olusturma/creating.js
@@ -1,26 +1,24 @@
 // let gorevlistesi = ["Görev 1", "Görev 2", "Görev 3", "Görev 4"];
 
-let gorevListesi = [
+let gorevlistesi = [
   { id: 1, gorevAdi: "Görev1" },
   { id: 2, gorevAdi: "Görev2" },
   { id: 3, gorevAdi: "Görev3" },
   { id: 4, gorevAdi: "Görev4" },
 ];
 
-ul = document.getElementById("task-list");
+let tasks = document.getElementById(`task-list`);
 
-// let count = ul.children.length;
-
-for (let item of gorevListesi) {
-  let li = `
-<li class="task list-group-item">
-<div class="form-check">
-    <input type="checkbox" id="${item.id}" class="form-check-input">
-    <label for="${item.id}" class="form-check-label">${item.gorevAdi}</label>
-<div>
-</li>
-`;
-  ul.insertAdjacentHTML("beforeend", li);
+for (let gorev of gorevlistesi) {
+  let taskShow = `
+  <li class="task list-group-item">
+  <div class="form-check">
+      <input type="checkbox" id="${gorev.id}" class="form-check-input">
+      <label for="${gorev.id}" class="form-check-label">${gorev.gorevAdi}</label>
+  <div>
+  </li>
+  `;
+  tasks.insertAdjacentHTML(`beforeend`, taskShow);
 }
 
 // insertAdjacentHTML =>HTML kodunu belirtilen bir konuma ekler.
